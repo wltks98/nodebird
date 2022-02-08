6831c5ac7f6850912aeb9ec344f42a490295aa8f
김지산 <79401359+wltks98@users.noreply.github.com>
2022-02-08 21:10:16 +0900

    Update user.js

--- models/__mocks__/user.js
+++ models/__mocks__/user.js
@@ -2,10 +2,10 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   findOne: async ({ where: { email, id } }) => {
-    if (email === 'zerohch0@gmail.com' || id === 1) {
+    if (email === 'wltks98@hanmail.net' || id === 1) {
       return {
         id: 1,
-        email: 'zerohch0@gmail.com',
+        email: 'wltks98@hanmail.net',
         password: await bcrypt.hash('nodejsbook', 12),
         Followers: [],
         Followings: [],
